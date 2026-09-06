22dc677b92b339ddcca61448e359e5e21752c813
Vektorel <Vektorel@LAB201-OGRETMEN>
2026-09-06 10:36:47 +0300

    Add project files.

---
+++ 06-09-2026/Human.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_09_2026
+{
+    internal class Human
+    {
+		private string _name;
+		private string _lastName;
+		private DateTime _birthDay;
+		private double _height;
+		private double _weight;
+
+        public Human(double weight, double height, DateTime birthDay, string lastName, string name)
+        {
+            Weight = weight;
+            Height = height;
+            BirthDay = birthDay;
+            LastName = lastName;
+            Name = name;
+        }
+
+        public double Weight
+		{
+			get { return _weight; }
+			set { 
+			if(value < 0)
+				{
+                    Console.WriteLine("Yok daha neler...");
+				}
+				else
+				{
+					_weight = value;
+				}
+			}
+		}
+
+
+		public double Height
+		{
+			get { return _height; }
+			set {
+                if (value < 0)
+                {
+                    Console.WriteLine("Yok daha neler...");
+                }
+                else
+                {
+                    _weight = value;
+                }
+            }
+		}
+
+
+		public DateTime BirthDay
+		{
+			get { return _birthDay; }
+			set { _birthDay = value; }
+		}
+
+
+		public string LastName
+		{
+			get { return _lastName; }
+			set { _lastName = value; }
+		}
+
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		public string info()
+		{
+			return $"Ad: {Name+" "+LastName}, Doğum Tarihi: {BirthDay} Boy: {Height} Kilo: {Weight}";
+		}
+
+	}
+}
---
+++ 06-09-2026/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_09_2026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human mahmut = new Human(80, 175, new DateTime(2000, 10, 15), "Keser", "Mustafa");
+
+            mahmut.Weight = -85;
+            Console.WriteLine(mahmut.info());
+
+
+            Console.ReadLine();
+        }
+    }
+}
